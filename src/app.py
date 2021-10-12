a96704cadd0cb513ac94328d595ed79b1a0fe8b5
Hernán de la Fuente <hernan.de@mercadolibre.com>
2021-10-12 20:22:45 -0300

    add docker and dependencies

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 from flask import Flask, jsonify, redirect, url_for
+from os import environ
+from celery import Celery
 
 
 from src.encoders import CustomJSONEncoder
@@ -14,6 +16,11 @@ from src.translations import gettext, pretty_date
 app = Flask(__name__)
 
 
+environ.setdefault('CELERY_CONFIG_MODULE', 'celery_config')
+
+celery_app = Celery()
+celery_app.config_from_envvar('CELERY_CONFIG_MODULE')
+
 app.register_blueprint(BACKOFFICE_BLUEPRINT, url_prefix="/backoffice")
 app.register_blueprint(BACKOFFICE_USER_BLUEPRINT, url_prefix="/backoffice_user")
 
@@ -24,12 +31,25 @@ app.jinja_env.globals.update(current_user=current_user)
 
 @app.route("/health-check")
 def health_check():
-    return jsonify({"status": "ok"}), HTTPStatus.OK
+    result = add.delay(1, 2)
+    resultado = result.get(timeout=1)
+    return jsonify({"status": resultado}), HTTPStatus.OK
 
 
 @app.errorhandler(HTTPStatus.NOT_FOUND)
 def page_not_found(e):
     return redirect(url_for("backoffice_controller.home")), HTTPStatus.MOVED_PERMANENTLY
+
+
+@celery_app.task
+def add(x, y):
+    return x + y
+
+@celery_app.task
+def tsum(*args, **kwargs):
+    print(args)
+    print(kwargs)
+    return sum(args[0])
 
 
 app.json_encoder = CustomJSONEncoder
